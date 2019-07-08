a4c1aa2f65eb201cf0bb895d12dce18486a8299e
Alexandros Antoniades <antoniades.alex@gmail.com>
2019-07-08 12:54:05 +0000

    Added code to main

---
+++ sros/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sros",
+    version="0.1",
+    license="Apache License, Version 2.0",
+    description="Swarm Robotics Orchestration System",
+    author="Alexandros Antoniades",
+    author_email="alex.rogue.antoniades@gmail.com",
+    url="https://github.com/alexantoniades/sros-automation",
+    packages=['sros']
+)
---
+++ sros/src/sros.py
@@ -0,0 +1,51 @@
+import sys
+import distro
+
+class System:
+    
+    operating_systems = {
+        "linux": "Linux",
+        "win32": "Windows",
+        "cygwin": "Cygwin",
+        "darwin": "MacOSX"
+        }
+    
+    os = operating_systems[sys.platform]
+    distribution = distro.id()
+    
+    def isSupported(self):
+        try:
+            out = {
+                "response": None,
+                "error": None
+            }
+            
+            if sys.platform.startswith('linux'):
+                out["error"] = "Linux is supported"
+                out["response"] = True
+                
+            elif sys.platform.startswith('win32'):
+                out["error"] = "Windows is not supported yet"
+                out["response"] = False
+                
+            elif sys.platform.startswith('cygwin'):
+                out["error"] = "Cygwin is not supported yet."
+                out["response"] = False
+                
+            elif sys.platform.startswith('darwin'):
+                out["error"] = "MacOSX is not supported yet"
+                out["response"] = False
+            
+            else:
+                out["error"] = sys.platform + ": Unknown OS not supported"
+                out["response"] = False
+                
+            return(out)
+        except:
+            print("Operating System is invalid")
+    
+    
+    
+system = System()
+
+print(system.isSupported()["response"])
